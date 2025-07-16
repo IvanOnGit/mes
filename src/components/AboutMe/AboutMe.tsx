48cd4dc5902d7f8dc72baeabd48a74ebd96d5499
Ivan Santamans <ivansantamans@gmail.com>
2025-07-16 15:33:39 -0300

    about me container and professional path sections in progress

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,6 +1,6 @@
 import Eugenia from "../Eugenia/Eugenia";
 import Navbar from "../Navbar/Navbar";
-import { AboutMeContainer, Container } from "./styles";
+import { AboutMeContainer, ButtonsContainer, Container, IconsAndButtons, IconsContainer, TitleAndIcons } from "./styles";
 
 function AboutMe() {
 
@@ -9,8 +9,70 @@ function AboutMe() {
         <Navbar />
         <Eugenia />
         <AboutMeContainer>
-          <h1>Sobre mi</h1>
+          <span></span>
+          <IconsAndButtons>            
+            <TitleAndIcons>
+              <h1>Sobre mi</h1>
+              <IconsContainer>
+                  <img src="/images/About me Icon 1.svg" alt="" />
+                  <img src="/images/About me Icon 2.svg" alt="" />
+                  <img src="/images/About me Icon 3.svg" alt="" />
+                  <img src="/images/About me Icon 4.svg" alt="" />
+                  <img src="/images/About me Icon 5.svg" alt="" />
+                  <img src="/images/About me Icon 6.svg" alt="" />
+                  <img src="/images/About me Icon 7.svg" alt="" />
+                  <img src="/images/About me Icon 8.svg" alt="" />
+                  <img src="/images/About me Icon 9.svg" alt="" />
+              </IconsContainer>
+            </TitleAndIcons>
+            <ButtonsContainer>
+              <button>
+                  <img src="/images/BioMagister.svg" alt="" />
+                  Bio - Magister
+              </button>
+              <button>
+                  <img src="/images/Traveller.svg" alt="" />
+                  Viajera
+              </button>
+              <button>
+                  <img src="/images/Investigator.svg" alt="" />
+                  Investigadora
+              </button>
+              <button>
+                  <img src="/images/Creator.svg" alt="" />
+                  Creadora
+              </button>
+              <button>
+                  <img src="/images/ChangeAgent.svg" alt="" />
+                  Agente de Cambio
+              </button>
+            </ButtonsContainer>
+          </IconsAndButtons>
         </AboutMeContainer>
+        <div>
+          <span></span>
+          <div>
+            <h1>RECORRIDO PROFESIONAL</h1>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <div>
+              <h2>Card1</h2>
+              <p>Lorem ipsum dolor sit amet</p>
+              <button>Ver más</button>
+            </div>
+            <div>
+              <h2>Card2</h2>
+              <p>Lorem ipsum dolor sit amet</p>
+              <button>Ver más</button>
+            </div>
+            <div>
+              <h2>Card3</h2>
+              <p>Lorem ipsum dolor sit amet</p>
+              <button>Ver más</button>
+            </div>
+          </div>
+        </div>
     </Container>
   );
 }
--- src/components/AboutMe/styles.ts
+++ src/components/AboutMe/styles.ts
@@ -16,6 +16,14 @@ export const AboutMeContainer = styled.div`
   flex-direction: column;
   overflow: hidden;
   margin-top: 40rem;
+  margin-bottom: 2rem;
+
+  span {
+    width: 44.5rem;
+    height: 10px;
+    margin-top: 7rem;
+    background-color: #1C1C1C;
+  }
 
   h1 {
     color: white;
@@ -23,5 +31,53 @@ export const AboutMeContainer = styled.div`
     font-weight: 200;
     margin: 0;
     padding: 0;
+  }
+`;
+
+export const IconsAndButtons = styled.div`
+  display: flex;
+  gap: 20rem;
+`;
+
+export const TitleAndIcons = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  gap: 4rem;
+`;
+
+export const IconsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 1rem;
+  width: 15rem;
+  height: 15rem;
+  justify-items: center;
+  align-items: center;
+  
+  img {
+    width: 4rem;
+    height: 4rem;
+    object-fit: contain;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 8rem;
+
+  button {
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid white;
+    color: white;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding-bottom: 0.5rem;
   }
 `;
