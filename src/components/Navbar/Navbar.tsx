1dfb819f70ba29ca1b2ccc673eb8a24a570c6ffc
Ivan Santamans <ivansantamans@gmail.com>
2025-08-07 20:10:01 -0300

    navbar responsiveness finished

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Container, ContainerLinks, Logo } from "./styles";
+import { Container, ContainerLinks, Logo, HamburgerMenu, HamburgerLine, MobileMenu } from "./styles";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <Container>
       <Logo src="/images/Logo.svg" alt="" />
+      
+      {/* Desktop Menu */}
       <ContainerLinks>
         <a>Inicio</a>
         <a>Servicios</a>
-        <Link to={'/sobre-mi'}>
-        <a>Sobre Mi</a>
-        </Link>
+        <Link to={'/sobre-mi'}>Sobre Mi</Link>
         <a>Contacto</a>
       </ContainerLinks>
+
+      {/* Hamburger Menu Button */}
+      <HamburgerMenu onClick={toggleMenu} $isOpen={isMenuOpen}>
+        <HamburgerLine />
+        <HamburgerLine />
+        <HamburgerLine />
+      </HamburgerMenu>
+
+      {/* Mobile Menu */}
+      <MobileMenu $isOpen={isMenuOpen}>
+        <a onClick={() => setIsMenuOpen(false)}>Inicio</a>
+        <a onClick={() => setIsMenuOpen(false)}>Servicios</a>
+        <Link to={'/sobre-mi'} onClick={() => setIsMenuOpen(false)}>Sobre Mi</Link>
+        <a onClick={() => setIsMenuOpen(false)}>Contacto</a>
+      </MobileMenu>
     </Container>
   );
 }
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -6,28 +6,39 @@ export const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   position: relative;
-  gap: 40rem;
   margin-top: 2rem;
   margin-bottom: 2rem;
+  padding: 0 1rem;
+
+  @media (min-width: 1025px) {
+    gap: 40rem;
+    padding: 0;
+  }
+
+  @media (max-width: 768px) {
+    gap: 2rem;
+  }
 `;
 
 export const Logo = styled.img`
-  margin-left: 6.81rem;
   width: 21rem;
 
-  @media (max-width: 768px) {
-    margin-left: 1rem;
+  @media (max-width: 1024px) {
     width: 15rem;
+  }
+
+  @media (min-width: 1025px) {
+    margin-left: 6.81rem;
   }
 `;
 
 export const ContainerLinks = styled.div`
   display: flex;
-  margin-right: 1rem;
   gap: 3rem;
   width: 30rem;
 
-  a {
+  a, 
+  a[href] {
     color: white;
     text-decoration: none;
     font-family: "Inter", sans-serif;
@@ -36,5 +47,78 @@ export const ContainerLinks = styled.div`
 
   @media (max-width: 1024px) {
     display: none;
+  }
+
+  @media (min-width: 1025px) {
+    margin-right: 1rem;
+  }
+`;
+
+export const HamburgerMenu = styled.div<{ $isOpen: boolean }>`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  padding: 5px;
+  z-index: 1001;
+
+  @media (max-width: 1024px) {
+    display: flex;
+  }
+
+  /* Animation for hamburger to X transformation */
+  ${({ $isOpen }) => $isOpen && `
+    div:nth-child(1) {
+      transform: rotate(45deg) translate(5px, 5px);
+    }
+    div:nth-child(2) {
+      opacity: 0;
+    }
+    div:nth-child(3) {
+      transform: rotate(-45deg) translate(7px, -6px);
+    }
+  `}
+`;
+
+export const HamburgerLine = styled.div`
+  width: 25px;
+  height: 3px;
+  background-color: white;
+  margin: 3px 0;
+  transition: 0.3s;
+  transform-origin: center;
+`;
+
+export const MobileMenu = styled.div<{ $isOpen: boolean }>`
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  z-index: 1000;
+  transform: ${({ $isOpen }) => $isOpen ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 1024px) {
+    display: flex;
+  }
+
+  a,
+  a[href] {
+    color: white;
+    text-decoration: none;
+    font-family: "Inter", sans-serif;
+    cursor: pointer;
+    font-size: 1.5rem;
+    padding: 1rem;
+    
+    &:hover {
+      color: #ccc;
+    }
   }
 `;
