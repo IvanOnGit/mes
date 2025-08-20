ee55d086bb6a776ab542cd252f1ef278a3b20da4
Ivan Santamans <ivansantamans@gmail.com>
2025-08-20 13:32:25 -0300

    spelling corrections

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -29,11 +29,11 @@ function Bio() {
         <div>
           <h2>Facilitar la Evolución Consciente</h2>
           <p><strong>Biotrascendencia</strong> es un enfoque cuyo propósito es facilitar la <strong>evolución consciente</strong>, con el objetivo de alcanzar una <strong>vida plena</strong>, trascendiendo las limitaciones de lo conocido.</p>
-          <p>El <strong>proceso de transformación personal y profesional</strong> logra que las personas se <strong>conecten con su escencia, su propósito</strong> y desarrollen la capacidad de <strong>liderazgo con sentido.</strong></p>
+          <p>El <strong>proceso de transformación personal y profesional</strong> logra que las personas se <strong>conecten con su esencia, su propósito</strong> y desarrollen la capacidad de <strong>liderazgo con sentido.</strong></p>
         </div>
         <div>
           <p>La consultoría le brinda a los Líderes las <strong>herramientas necesarias para alcanzar sus metas profesionales</strong> y prosperar en entornos complejos, dinámicos y cambiantes.</p>
-          <p>Adempas de fomentar un cambio profundo en uno mismo, este enfoque tiene un <strong>impacto directo</strong> en el entorno socioambiental, ya que permite generar un <strong>efecto positivo y regenerativo</strong> en el mundo, contribuyendo a la <strong>creación de un ecosistema virtuoso.</strong></p>
+          <p>Además de fomentar un cambio profundo en uno mismo, este enfoque tiene un <strong>impacto directo</strong> en el entorno socioambiental, ya que permite generar un <strong>efecto positivo y regenerativo</strong> en el mundo, contribuyendo a la <strong>creación de un ecosistema virtuoso.</strong></p>
         </div>
         <img src="/images/Fiveimpacts.png" alt="" />
       </TextContainer>
