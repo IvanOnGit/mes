168c3b28079be41e792ff98533ea153602aee332
Ivan Santamans <ivansantamans@gmail.com>
2025-08-22 20:23:55 -0300

    floating title margin added

--- src/components/FloatingSubtitle/styles.ts
+++ src/components/FloatingSubtitle/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     position: absolute;
-    margin-top: 39rem;
+    margin-top: 41rem;
 
     h2 {
         color: white;
